2fb17e10b0f6fd997edf9d70261cbe1bd15280f3
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-06-23 17:04:23 +0200

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        let arr: string[] = [];
+        for (let i = 0; i < s.length; i++) {
+            arr.push(s[i]);
+            let num: number = 0;
+            if (t.includes(arr[i])) {
+                num = t.indexOf(arr[i]);
+                t.slice(num, num + 1);
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
