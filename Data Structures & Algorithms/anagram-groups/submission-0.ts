026f13f82700f88c72b08268c584e6b818152243
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-06-28 15:01:14 +0200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.ts
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        if(strs.length===0){
+            return [[""]];
+        }
+    }
+}
