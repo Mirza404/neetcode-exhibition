755546fc7833e327d01a938e8511988cd2455cb5
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-07-04 12:39:03 +0200

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.ts
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums: number[]): number {
+        let i: number = 0;
+        let maxNumOfCons: number = 0;
+        let currentNumOfCons: number = 0;
+        // let tempIsCons: boolean = false;
+
+        // for (i; i < nums.length; i++) {
+        //     if (nums[i] === 1) {
+        //         tempIsCons = true;
+        //     } else {
+        //         numOfCons = 1;
+        //         tempIsCons = false;
+        //         continue;
+        //     }
+        //     if (tempIsCons === true && nums[i+1]===1) {
+        //         numOfCons++;
+        //     }
+        // }
+
+        for (i; i < nums.length; i++) {
+            if (nums[i] === 1) {
+                currentNumOfCons++;
+            } else {
+                currentNumOfCons = 0;
+            }
+            if (currentNumOfCons > maxNumOfCons) {
+                maxNumOfCons = currentNumOfCons;
+                continue;
+            }
+        }
+
+        return maxNumOfCons;
+    }
+}
