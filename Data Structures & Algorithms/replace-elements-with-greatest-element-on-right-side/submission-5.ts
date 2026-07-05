11f5acd4cde638cc78de3fa64992a7716da56e9a
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-07-05 10:24:50 +0200

    Add: replace-elements-with-greatest-element-on-right-side - submission-5

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-5.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+
+    // 1) pocnes od  zadnjeg elementa
+    // 2) pogledas arr[i], ako je zadnji, daj mu -1 i stavi localMax=arr[i]
+    // 3) onda ides i--, element nize uporedis sa localMaxom
+    // 4) Ako je veci arr[i], update localMax to match
+    // 5) Ako je localMax sad veci od totalMaxa, update it
+    replaceElements(arr: number[]): number[] {
+        let i: number = arr.length - 1;
+        let localMax: number = 0;
+        let totalMax: number = 0;
+        let ans: number[] = [];
+
+        for (i; i >= 0; i--) {
+            if (arr[i] > localMax) {
+                localMax = arr[i];
+            }
+
+            if (localMax > totalMax) {
+                totalMax = localMax;
+            }
+            ans[i-1] = totalMax;
+        }
+        ans[arr.length - 1] = -1;
+
+        return ans;
+    }
+}
