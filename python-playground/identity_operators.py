92b4d88d1e302aa9cbdd9afd1ffa88f8894eb741
Mirza Abdulahovic <mirza.abdulahovic@advanceddri.com>
2026-07-07 13:30:38 +0200

    important distinction

--- python-playground/identity_operators.py
+++ python-playground/identity_operators.py
@@ -2,6 +2,6 @@ x = ["apple", "banana"]
 y = ["apple", "banana"]
 z = x
 
-print(x is z)
-print(x is y)
-print(x == y)
+print(x is y) # Checks if both variables point to the same object in memory
+print(x is z) # Checks if both variables point to the same object in memory
+print(x == y) #checks if the values of both variables are equal
