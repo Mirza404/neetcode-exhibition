b3ecc2baf1305fad4b325d05853197baa686a0d9
Mirza Abdulahovic <mirza.abdulahovic@advanceddri.com>
2026-07-07 11:49:46 +0200

    having fun

--- python-playground/strings.py
+++ python-playground/strings.py
@@ -7,4 +7,15 @@ print(len(x))
 
 print(x[2: 5]) ## Easier slice -> O(k)
 
-print(x[3:])
+print(x[3:5])
+
+y="   uppercase"
+print(y.upper())
+
+print(y.strip()) # Exact same as trim in JS
+
+age = 36
+
+txt = (f"my name is mirza and my age is {age}. And I also got something to say: {x}")
+
+print(txt)
