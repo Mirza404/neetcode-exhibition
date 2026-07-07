83d4946b8408921d52b4ac9ab8a22641fc45fcfd
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-07-07 15:31:00 +0200

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.ts
@@ -0,0 +1,48 @@
+class MinStack {
+    stack: number[];
+    minStack: number[];
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val);
+        if (this.minStack.length === 0) {
+            this.minStack.push(val);
+        } else if (this.minStack.at(-1) >= val) {
+            this.minStack.push(val);
+        } else {
+            this.minStack.push(this.minStack.at(-1));
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        if (this.minStack.at(-1) === this.stack.at(-1)) {
+            this.minStack.pop();
+        }
+        this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack.at(-1);
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        if(this.minStack.length === 0) return 0;
+        return this.minStack.at(-1);
+    }
+}
